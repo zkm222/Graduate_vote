f74808a6f295e7c188958a4c3b242872656fdef3
tanliqiu2 <112788612+tanliqiu2@users.noreply.github.com>
2023-09-10 23:02:50 +0800

    Update index.jsx

--- src/compoments/show/totalcount/index.jsx
+++ src/compoments/show/totalcount/index.jsx
@@ -1,15 +1,40 @@
 import React from "react"
 import styles from './totalcount.module.css'
-var count = {
-    total: 10,
-    now: 5
+import { Component } from 'react';
+import axios from "axios";
+class Totalcount extends Component{
+state={
+    teachersNum:'',
+    teachersAll:'10'
 }
-const Totalcount = (() => {
+
+componentDidMount(){
+    axios({
+        method:'post',//请求方式
+        url:'http://43.140.197.15:8080/admin/getVoteResult',//请求地址
+        params:'',//和url一起发送的数据（如get请求）
+        data:'',//必要参数，
+        // 自定义请求头
+        }).then(
+        res=>{
+             if(res.data.msg == 'success'){
+             }
+             else{
+                console.log('failed')
+             }
+            this.setState({
+                teachersNum:res.data.data.teachersNum
+            })
+            console.log(this.state.teachersNum)
+        })
+}
+render(){
     return (
         <div>
-            <div className={styles.total}>当 前 投 票 人 数 :{'\u00A0\u00A0\u00A0'}<span className={styles.count}> {count.now} / {count.total}</span></div>
+            <div className={styles.total}>当 前 投 票 人 数 :{'\u00A0\u00A0\u00A0'}<span className={styles.count}> {this.state.teachersNum} / {this.state.teachersAll}</span></div>
         </div>
 
     )
-})
+}
+}
 export default Totalcount
