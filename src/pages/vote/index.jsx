08ba6b686aa46d4122e1b068222914fccb46bf4f
kemingzhang <3161557275@qq.com>
2023-09-12 13:36:35 +0800

    qwq

--- src/pages/vote/index.jsx
+++ src/pages/vote/index.jsx
@@ -1,12 +1,11 @@
 import Form from "../../compoments/vote/form"
 import Header from "../../compoments/vote/header"
-import Footer from "../../compoments/vote/footer"
 const Vote=(()=>{
     return (
         <div>
             <Header></Header>
             <Form></Form>
-            <Footer></Footer>
+            
         </div>
     )
 })
